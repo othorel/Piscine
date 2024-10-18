c6a9bdc34d9395e6642ee2f1e808a045d4655e10
othorel <thorel.olivier@hotmail.com>
2024-10-18 20:13:01 +0200

    C00 ex05

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: othorel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 10:27:32 by othorel           #+#    #+#             */
+/*   Updated: 2024/07/18 14:31:07 by othorel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	write(1, "\n", 1);
+}*/
