46114371b2a1aa38cf27496bc8e979329a281043
othorel <thorel.olivier@hotmail.com>
2024-10-23 15:26:36 +0200

    C09 ok

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: othorel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 16:15:02 by othorel           #+#    #+#             */
+/*   Updated: 2024/08/01 13:36:26 by othorel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <stdio.h>
+
+int	is_charset(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (charset[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_word(char *str, char *charset)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		while (*str && is_charset(*str, charset))
+			str++;
+		if (*str && !is_charset(*str, charset))
+		{
+			count++;
+			while (*str && !is_charset(*str, charset))
+				str++;
+		}
+	}
+	return (count);
+}
+
+char	*malloc_word(char *str, char *charset)
+{
+	int		i;
+	char	*word;
+
+	i = 0;
+	while (str[i] && !is_charset(str[i], charset))
+		i++;
+	word = malloc(sizeof(char) * (i + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (str[i] && !is_charset(str[i], charset))
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int		i;
+	char	**split;
+
+	i = 0;
+	split = (char **)malloc(sizeof(char *) * (count_word(str, charset) + 1));
+	if (!split)
+		return (NULL);
+	while (*str)
+	{
+		while (*str && is_charset(*str, charset))
+			str++;
+		if (*str && !is_charset(*str, charset))
+		{
+			split[i] = malloc_word(str, charset);
+			i++;
+			while (*str && !is_charset(*str, charset))
+				str++;
+		}
+	}
+	split[i] = NULL;
+	return (split);
+}
+/*
+int	main(void)
+{
+	char	*str = "Hello//world//!";
+	char	*charset = "/";
+	int		i = 0;
+	char	**split = ft_split(str, charset);
+
+	while (split[i])
+	{
+		printf("%d : %s\n", i, split[i]);
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (0);
+}*/
